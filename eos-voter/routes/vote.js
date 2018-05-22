3c0c085e575b83406f491a17987c0d6fba4d9b18
Mark Lockett <mlockett@bigpond.com>
2018-05-22 11:42:37 +0800

    Change presentation of vote total to something people can read

--- eos-voter/routes/vote.js
+++ eos-voter/routes/vote.js
@@ -22,12 +22,12 @@ function ValidURL(str) {
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     let active_block_producers = chaininpector.get_active_block_producers().map((x) => {
-        return { 'id': x.owner, 'name': x.owner, 'votes': x.total_votes,
+        return { 'id': x.owner, 'name': x.owner, 'votes': parseFloat(x.total_votes).toPrecision(4),
                   'statement': x.url, 'valid_url': ValidURL(x.url),
                   'last_produced_block_time': x.last_produced_block_time };
     });
     let backup_block_producers = chaininpector.get_backup_block_producers().map((x) => {
-        return { 'id': x.owner, 'name': x.owner, 'votes': x.total_votes,
+        return { 'id': x.owner, 'name': x.owner, 'votes': parseFloat(x.total_votes).toPrecision(4),
                   'statement': x.url, 'valid_url': ValidURL(x.url),
                   'last_produced_block_time': x.last_produced_block_time };
     });
