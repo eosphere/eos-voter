4d64ad5410932100225c7296f6a3a73ed3416e30
Mark Lockett <mlockett@bigpond.com>
2018-05-23 13:18:10 +0800

    Remove unnecessary strcmp function

--- eos-voter/routes/index.js
+++ eos-voter/routes/index.js
@@ -4,21 +4,6 @@ var chaininpector = require('../tasks/chainInspector')
 var utils = require('../utils/utils.js');
 var settings = require('../config/settings.js');
 
-
-function strcmp ( str1, str2 ) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Waldo Malqui Silva
-    // +      input by: Steve Hilder
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +    revised by: gorthaur
-    // *     example 1: strcmp( 'waldo', 'owald' );
-    // *     returns 1: 1
-    // *     example 2: strcmp( 'owald', 'waldo' );
-    // *     returns 2: -1
-
-    return ( ( str1 == str2 ) ? 0 : ( ( str1 > str2 ) ? 1 : -1 ) );
-}
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
