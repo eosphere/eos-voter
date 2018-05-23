17ffb5d18bcb0c9f1be5dbfaee66d5b94727b3b2
Mark Lockett <mlockett@bigpond.com>
2018-05-23 10:14:20 +0800

    Add a more releasistic algoritm for calculating the who is active and who is backup

--- eos-voter/tasks/chainInspector.js
+++ eos-voter/tasks/chainInspector.js
@@ -30,13 +30,23 @@ var db = null;
 var block_producers_collection = [];
 var active_block_producers = [];
 var last_irreversible_block_num = 0;
+var recent_blocks = {};
+
+function get_recent_producers() {
+    var blocks = Object.keys(recent_blocks).map((key) => recent_blocks[key]);
+    return blocks.map((block) => block.producer);
+}
 
 exports.get_active_block_producers = function() {
-    return active_block_producers.filter((x) => { return x.last_produced_block_time >= (last_irreversible_block_num - 63) });
+    var recent_producers = get_recent_producers();
+    console.log('get_active_block_producers recent_producers=', recent_producers);
+    active_block_producers.filter((producer) => { return console.log('producer=', producer); });
+    return active_block_producers.filter((producer) => { return recent_producers.includes(producer.owner); });
 };
 
 exports.get_backup_block_producers = function() {
-    return active_block_producers.filter((x) => { return x.last_produced_block_time < (last_irreversible_block_num - 63) });
+    var recent_producers = get_recent_producers();
+    return active_block_producers.filter((producer) => { return !recent_producers.includes(producer.owner); });
 };
 
 console.log('Attempting to connect to mongodb');
@@ -116,6 +126,7 @@ function inspectChain()
         (result) => {
         console.log('getInfo returned');
         last_irreversible_block_num = result.last_irreversible_block_num;
+        var head_block_num = result.head_block_num;
         eos.getTableRows({'json': true, 'code': 'eosio', 'scope': 'eosio', 'table': 'producers', 'limit': 500}).then(
             (result) => {
                 console.log('getTableRows returned');
@@ -136,6 +147,37 @@ function inspectChain()
                  setTimeout(inspectChain, 5 * 1000);
             }
             );
+        // Iterate over recent blocks. We consider a BP to be active if it has recently produced a block
+        // Note the algorithm may strangely change the state if the elected BP's have changed since the 
+        // last iireversible block and there has been a chain fork
+
+        console.log(Object.keys(recent_blocks).length, ' blocks in the recent block list');
+
+        // Firstly trim the list of blocks to get rid of old ones.
+        var blocks_to_remove = [];
+        Object.keys(recent_blocks).forEach(function(key) {
+            if (key < last_irreversible_block_num - 63) {
+                //console.log('Getting block to trim', key);
+                blocks_to_remove.push(key);
+                }
+            });
+
+        // Delete old blocks from the in memory list
+        for (let i = 0 ; i < blocks_to_remove.length ; i++) {
+            //console.log('Trimming key ', blocks_to_remove[i]);
+            delete recent_blocks[blocks_to_remove[i]];
+        }
+
+        // Download the blocks we are missing
+        var blocks_to_download = [];
+        for (let i = head_block_num; i >= last_irreversible_block_num - 63 && i >= 0 ; i--) {
+            if (!(i in recent_blocks)) {
+                //console.log('Block ', i, ' not in the list downloading it');
+                eos.getBlock(i).then((result) => {
+                                                   recent_blocks[result.block_num] = result;
+                                                 });
+            }
+        }
     }).catch(
         (result) => {
                     console.error('Error result=', result);
