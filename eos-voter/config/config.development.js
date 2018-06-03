216170b8fc2bd57b9bb565bf689495455bbd3906
Mark Lockett <mlockett@bigpond.com>
2018-06-03 19:47:45 +0800

    Frontend ssl testing

--- eos-voter/config/config.development.js
+++ eos-voter/config/config.development.js
@@ -3,21 +3,19 @@ var config = require('./config.global');
 // Jungle TestNet
 // IP address or Domain of the API end point
 
-
+/*
 config.chain_addr = 'dev.cryptolions.io'
 // Port to connect to the API
 config.chain_port = '38888'
 // Protocol to use. Valid choices are http or https
 config.protocol = 'http'
+*/
 
-
-/*
 config.chain_addr = 'node1.eosphere.io'
 // Port to connect to the API
 config.chain_port = '443'
 // Protocol to use. Valid choices are http or https
 config.protocol = 'https'
-*/
 
 /*
 config.chain_addr = 'bp.libertyblock.io'
