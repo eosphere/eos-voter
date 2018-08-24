00b49c2e16580c328b9298a30ab073a8f1cab01f
Mark Lockett <mark@deliveryengine.net>
2018-08-24 13:54:10 +0800

    Get the chain inspector to regularly poll the block chain and to connect to the mongodb database

--- chaininspector/chaininspector.py
+++ chaininspector/chaininspector.py
@@ -1,6 +1,18 @@
 #! /usr/bin/python3
 import time
+from pymongo import MongoClient
+from eosapi import Client
+
+c = Client(nodes=['https://node2.eosphere.io'])
+
+client = MongoClient('mongodb://mongo:27017/')
+db = client.eos_producers
+producers = db.producers
 
 while True:
-    print('Inspect chain')
+    print(c.get_table_rows(json= True, code= 'eosio', scope= 'eosio',
+                     table= 'producers', limit=1500, table_key=1,
+                     lower_bound=0, upper_bound=-1))
+
+    #print('Inspect chain')
     time.sleep(1)
