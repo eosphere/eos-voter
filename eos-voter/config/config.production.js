13b065a94878a5135f77f0dbb0f1aeb2c71ea423
Mark Lockett <mark@deliveryengine.net>
2019-01-29 14:22:42 +0800

    Connect production to the correct api end point

--- eos-voter/config/config.production.js
+++ eos-voter/config/config.production.js
@@ -1,6 +1,6 @@
 var config = require('./config.global');
 
-config.chain_addr = 'node1.eosphere.io'
+config.chain_addr = 'telos.eosphere.io'
 // Port to connect to the API
 config.chain_port = '443'
 // Protocol to use. Valid choices are http or https
@@ -9,7 +9,7 @@ config.protocol = 'https'
 config.chain_secure_port = '443'
 
 // User readable name for the chain
-config.chain_name = 'EOS Mainnet';
+config.chain_name = 'Telos Mainnet';
 
 config.mongodb_server = 'localhost';
 
